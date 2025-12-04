2842e76ba68f343f61b5936c2be66d0cb90c1e6d
afidhu <afidhumatata81@gmail.com>
2025-12-05 01:00:03 +0300

    added REST framework and student_app urls

--- backend/student_app/urls.py
+++ backend/student_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.StudentList.as_view(), name='student-list'),
-    path('<int:pk>/', views.StudentDetail.as_view(), name='student-detail'),
-]
+# urlpatterns = [
+#     path('', views.StudentList.as_view(), name='student-list'),
+#     path('<int:pk>/', views.StudentDetail.as_view(), name='student-detail'),
+# ]
