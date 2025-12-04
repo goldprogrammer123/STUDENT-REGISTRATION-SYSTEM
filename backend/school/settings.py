fd8d95a9bbe9e39af205fcd4d8c8ca41f281a58b
mwaitukasteven <mwaitukasteven@gmail.com>
2025-12-04 22:26:45 +0300

    register app to the settings and change database to postgresql

--- backend/school/settings.py
+++ backend/school/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student_app',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'school.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': ' ',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
